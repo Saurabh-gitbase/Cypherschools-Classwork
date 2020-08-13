13b80f2d291d286930cfcca1aa711e16c9cae0bf
Saurabh <49409857+Saurabh-gitbase@users.noreply.github.com>
2020-08-13 11:27:46 +0530

    Create stack.js

---
+++ stack.js
@@ -0,0 +1,54 @@
+class Stack {
+
+    constructor() 
+    { 
+        this.items = []; 
+    }
+
+    push(element) 
+    {  
+      this.items.push(element); 
+    } 
+
+    pop() 
+    { 
+    if (this.items.length == 0) 
+        return "Stack is empty"; 
+    return this.items.pop(); 
+    } 
+ 
+    top() 
+    { 
+    return this.items[this.items.length - 1]; 
+    } 
+
+    isEmpty() 
+    {  
+    return this.items.length == 0; 
+    } 
+
+    display() 
+    { 
+    let str = ""; 
+    for (let i = 0; i < this.items.length; i++) 
+        str += this.items[i] + " "; 
+    return str; 
+    }
+}
+
+var stack = new Stack();
+
+stack.push(97); 
+stack.push(37); 
+stack.push(21);
+stack.push(7);
+
+stack.top();
+
+stack.display();
+
+stack.pop();
+
+stack.display();
+
+stack.isEmpty();
